6d6e213e17499c571921fb9f3a262060779e9cfc
HadoopSubbu <subhashmantha@gmail.com>
2014-04-22 15:50:23 -0500

    Added The code for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	## Initialize inverse to be a null matrix
+    inv <- NULL 
+	
+	## Assign data to matrix x using input
+	setmatrix<- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+	## Return Matrix
+	getmatrix<- function() x
+	
+	## Calculate and assign Inverse
+	setinv <- function(solve) inv <<- solve	
+	
+	## Get the Inverse
+	getinv <- function() inv
+	
+	## List of methods
+	list(setmatrix=setmatrix,
+	     getmatrix=getmatrix,
+		 setinv=setinv,
+		 getinv=getinv)
 
 }
 
@@ -12,4 +34,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		## Check if matrix is Square
+		if (dim(x$getmatrix())[1] == dim(x$getmatrix())[2])
+		{
+			## get the Inverse. Need not exist at this point
+			inv <- x$getinv()
+			## Check if the Inverse is already calculated
+			if(!is.null(inv)) {
+					message("getting cached data")
+					return(inv)
+			}
+			## Get matrix
+			data <- x$getmatrix()
+			## Calculate Inverse
+			inv <- solve(data)
+			## Set it for later use
+			x$setinv(inv)
+			## Return the inverse
+			inv
+		}
+		else {
+			message("Cannot Calculate Inverse as the matrix is not a square")
+            return(NULL)
+		}
 }
